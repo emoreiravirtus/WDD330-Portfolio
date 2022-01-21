0fc52a6d7a86b729dc4d64f3dae1dc6084f16a1d
Elias Moreira <emoreiravirtus@gmail.com>
2022-01-20 22:14:55 -0300

    Adds week3 notes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
-
-
-const links = [ 1, 2 ]
+const links = [ 1, 2, 3 ]
 
 for (link in links) {
-
-
-
     document.querySelector('body ol').innerHTML += `
         <li>
             <a href='week${parseInt(link) + 1}/index.html'>Week ${parseInt(link) + 1} Report</a>
